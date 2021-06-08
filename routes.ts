25241fa82b6a7eab1760151c7c537e440933a427
João Victor <santosjoao.dev@gmail.com>
2021-06-08 15:02:31 -0300

    feat: set user post route

--- routes.ts
+++ routes.ts
@@ -1,5 +1,8 @@
-import { Router } from "express";
+import { Router } from "express"
+import UserController from "./src/app/Controllers/UserController"
 
 const routes = Router()
+
+routes.post('/users', UserController.store)
 
 export default routes
