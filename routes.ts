7af7fbec03a1634367b8de6df3660abfa4b2562d
João Victor <santosjoao.dev@gmail.com>
2021-06-08 16:24:38 -0300

    feat: add authenticate route and users route

--- routes.ts
+++ routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express"
+import AuthController from "./src/app/Controllers/AuthController"
 import UserController from "./src/app/Controllers/UserController"
+import authMiddleware from "./src/app/Middlewares/authMiddlewares"
 
 const routes = Router()
 
 routes.post('/users', UserController.store)
+routes.post('/authenticate', AuthController.authenticate)
+routes.get('/users', authMiddleware, AuthController.index)
 
 export default routes
